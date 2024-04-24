47b8fad8ac63f6d2fe83e260faf70f0965e80dd3
Michael Roper <michael.roper@unitigrouplimited.com>
2024-04-24 22:11:27 +0800

    test routes

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,7 +1,13 @@
 export const prerender = false;
 
 export async function GET(context) {
-  return new Response("Hi, I'm an SSR API route.", {
-    status: 200,
+  return new Response("Hi, I'm an SSR API GET route.", {
+    status: 200
+  });
+}
+
+export async function POST(context) {
+  return new Response("Hi, I'm an SSR API POST route.", {
+    status: 200
   });
 }
